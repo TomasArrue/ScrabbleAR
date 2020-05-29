b55d7564817aaa6f2de347f6f59b82e0459d09d9
DaniloPaz01 <62101548+DaniloPaz01@users.noreply.github.com>
2020-05-28 21:38:47 -0300

    Add files via upload

--- tocado_por_Dani_e_e.py
+++ tocado_por_Dani_e_e.py
@@ -36,9 +36,14 @@ window = sg.Window('SCRABBLE', layout, default_button_element_size=(2,2), auto_s
 
 #event, values = window.read()
 
+#lleva las letras que ya use
 usados = []
+
+#lleva la cuenta de los lugares que ya escribi
 disponibles = []
 
+#para despintar la casilla anterior cuando toco una nueva
+ant = ()
 
 while True:
     event, values = window.read()
@@ -48,35 +53,53 @@ while True:
     else:
          if type(event) is tuple:
              lugar = event
+
+             #pinto el lugar que estoy seleccionando
              window[lugar].update(button_color=('white','skyblue'))
+
+             #digo que si anterior tiene algo que despinte lo anterior
+             if (ant):
+                 window[ant].update(button_color=('grey','white'))
+             ant = lugar
          #print(type(event))
          #print(event) #aca puedes ver que objeto esta recibiendo event, antes recibia una tupla
-         if event in a:
+         #si el evento seria una letra y lugar tiene algo es xq marque algo del tabler
+         if event in a and lugar:
+                 #asigno la letra del evento
                  letra = event
+                 #si el lugar no lo use
                  if lugar not in disponibles:
+                     #pinto de verde
                      window[lugar].update(letra, button_color=('white','green'))
                      print(a)
+                     #saco la letra de la bolsa
                      a.remove(letra)
                      print(a)
+                     #saco el boton de esa letra
                      window[letra].update(visible = False)
+                     #lo agrega a mi lista de usados
                      usados.append(letra)
+                     #cargo el lugar que ya use
                      disponibles.append(lugar)
                  else:
+                     #puede ser que toque un boton que ya tenia algo entonces lo dejo como esta
                      window[lugar].update(button_color=('white','green'))
          if event == "PEDIR FICHAS":
              a = letrasRandom()
+             #ZIP lo que hace es crear una lista de tuplas con las listas que le pasas
              mezcla = zip(usados,a)
              print(mezcla)
              for elem in mezcla:
+                 #busco la letra que use,en en ese lugar hago visible el boton y actualizo su texto
                  window[elem[0]].update(elem[1],visible=True)
          elif 'COMENZAR' == event :
                      # event, values = window.read()
                      window[a[0]].update('sh', button_color=('white','blue'))
                      asignarValores(window)
-                     
-
-                     # print(event)
          print(event)
+         letra2 = event
+         print(letra2)
+         print(values)
 window.close() 
 
     
