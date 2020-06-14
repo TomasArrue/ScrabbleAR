fd41c404125566b02e7cfcf7abb221e69bb1b8bb
TomasArrue <tomasaugustoarrue@gmail.com>
2020-06-13 22:22:41 -0300

    aclare tres bobadas de la fun cargar, depues sigo

--- funciones_para_palabra_y_tablero.py
+++ funciones_para_palabra_y_tablero.py
@@ -8,7 +8,14 @@ def validar
 
 def cargar_una_letra(palabra, tablero, x, y, letra):
 """ realiza todas la operaciones de cargar una letra y sumarlos puntos asociados """
-    coord = (x, y)
+# esta funcion ya toma como valido el espacio de celda,
+# ejemplo:
+# if var_tablero.celda_vacia(x, y):
+#     cargar_letra(var_palabra, var_tablero, x, y, letra)
+###############################################################################################
+# !!!!!!!!!!!! esto podria cambiar y las coords pueden validarse de otra manera !!!!!!!!!!!!!!!
+###############################################################################################
+    coord = (x, y)                          # guarde las coordenadas en una tupla
     tablero.cargar_letra(x, y, letra)       # carga la letra en el tablero
     tupla_puntos = tablero.sumar(x, y)      # guarda los puntos de la celda en una tupla
     palabra.sumar(tupla_puntos)             # suma los puntos y el multiplicador de palabra
