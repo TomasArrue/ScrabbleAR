06dbe53b988fe7fc1ea0133565d68923ec1f8920
LUCAS <lucaspincha@outlook.com>
2020-06-14 21:26:17 -0300

    CAMBIOS

--- tablero_en_crudo.py
+++ tablero_en_crudo.py
@@ -49,6 +49,25 @@ def asignarValores(window):
                     coordenadas_rojo.append(aux_cord2)
 
 
+def cargar_juego(window,timer_running):
+    asignarValores(window)
+    window['opcionesJuego'].update(visible=True)
+    window['Comenzar'].update(visible=False)
+    window['Cargar Partida'].update(visible=False)
+    window['Guardar Partida'].update(visible=True)
+    window['Salir'].update(visible=True)
+    window['puntaje'].update(visible=True)
+    window['atrilFichasRival'].update(visible=True)
+    window['atrilFichas'].update(visible=True)
+    window['dificultad'].update(visible=False)
+
+    for i in range(7):#carga de las 7 fichas al inicio
+        nro_de_boton='Boton_'+str(i+1)
+        obtener_fichas(window,nro_de_boton,a)
+    timer_running = not timer_running
+    return timer_running
+
+
 def volverAPintar(cord,window):
     if cord in coordenadas_rojo: window[cord].update(button_color=('indianred','indianred')) 
     elif cord in coordenadas_dorado: window[cord].update(button_color=('goldenrod','goldenrod')) 
@@ -98,24 +117,47 @@ def quitar_fichas(window,usados:list,botones_usados:list,no_disponibles:list):
     else:
         sg.Popup('No hay fichas para borrar')   
 
+
+def pedir_fichas(window,usados:list):
+    a = letrasRandom()
+    #ZIP lo que hace es crear una lista de tuplas con las listas que le pasas
+    mezcla = zip(usados,a)
+    print(mezcla)
+    for elem in mezcla:
+        #busco la letra que use,en en ese lugar hago visible el boton y actualizo su texto
+        window[elem[0]].update(elem[1],visible=True)
+
+
+def letra_al_tablero(window,usados,botones_usados,a,no_disponibles):
+    usados.append(letra) #lo agrega a mi lista de usados
+    botones_usados.append(event)#agrego el nombre del boton para luego recuperarlo
+    window[lugar].update(letra, button_color=('white','green'))#pinto de verde
+    a.remove(letra) #saco la letra de la bolsa
+    window[event].update(visible = False) #saco el boton de esa letra
+    no_disponibles.append(lugar)#cargo el lugar que ya use
+
+
 color_De_Boton=('Black','seagreen')
 tamanio_Boton_De_Fichas = 2,2 #tamanio de botones que seran las fichas
 tamanio_Boton_De_Control = 15,1 #tamanio de botones de comenzar y salir
-max_Cant_Filas = max_Cant_Columnas = 11 #tamanio de las matrices
+max_Cant_Filas = max_Cant_Columnas = 15 #tamanio de las matrices
+dificultad = ['Facil','Medio','Dificil']
 botones_De_Fichas = lambda name : sg.Button(name,button_color=color_De_Boton,size=tamanio_Boton_De_Fichas)
 botones_De_Fichas_rival = lambda name : sg.Button('?',button_color='color_De_Boton',size=tamanio_Boton_De_Fichas)
 sg.ChangeLookAndFeel('DarkGrey6')
 
 opciones_de_inicio = [ [sg.Button("Comenzar",size=tamanio_Boton_De_Control)],
                        [sg.Button("Cargar Partida",size=tamanio_Boton_De_Control)], 
+                       [sg.InputCombo(dificultad,default_value='Facil', size=(10, 10),key='dificultad')],
                        [sg.Button('Guardar Partida',size=tamanio_Boton_De_Control,visible=False)],      
-                       [sg.Button('Salir',size=tamanio_Boton_De_Control,visible=False)] 
+                       [sg.Button('Salir',size=tamanio_Boton_De_Control)] 
                      ]
 
 opciones_de_juego = [ [sg.Button('Borrar',size=tamanio_Boton_De_Control),
                       sg.Button("Pedir Fichas",size=tamanio_Boton_De_Control),
                       sg.Button("Evaluar",size=tamanio_Boton_De_Control),
-                      sg.Button("Repartir De Nuevo",size=tamanio_Boton_De_Control)]
+                      sg.Button("Repartir De Nuevo",size=tamanio_Boton_De_Control),
+                      sg.Button("TOP",size=tamanio_Boton_De_Control)]
                     ]                     
 
 fichas=[ [sg.Text("Tus Fichas: ",font=("Chalkboard", 15))],
@@ -132,7 +174,7 @@ fichas_rival =[ [sg.Text("Fichas CPU: ",font=("Chalkboard", 15))],
                 [botones_De_Fichas_rival(j) for j in b]
               ]  
 
-tablero=[ [sg.Button('',button_color=('grey','white'),size=(2, 2), key=(i,j), pad=(0,0)) for j in range(max_Cant_Columnas)] for i in range(max_Cant_Filas)]#botones matriz
+tablero=[ [sg.Button('',button_color=('grey','white'),size=(1, 1), key=(i,j), pad=(0,0)) for j in range(max_Cant_Columnas)] for i in range(max_Cant_Filas)]#botones matriz
 
 puntaje_y_tiempo=[ [sg.Text('TU PUNTAJE: 0',font=("Chalkboard", 15))],
                    [sg.Text('PUNTAJE PC: 0',font=("Chalkboard", 15))], 
@@ -142,7 +184,7 @@ puntaje_y_tiempo=[ [sg.Text('TU PUNTAJE: 0',font=("Chalkboard", 15))],
                  ]
 
 layout=[
-       [sg.Text("ScrabbleRoll", size=(10, 1), justification='left', font=("Chalkboard", 25), relief=sg.RELIEF_RIDGE)],
+       [sg.Text("Scrabble", size=(8, 1), justification='left', font=("Chalkboard", 25), relief=sg.RELIEF_RIDGE)],
        [sg.Column(fichas_rival,key='atrilFichasRival',justification='center',visible=False)],
        [sg.Column(opciones_de_inicio,key='opcionesComienzo',justification='left'), sg.Column(tablero),sg.Column(puntaje_y_tiempo,key='puntaje',visible=False)],
        #[sg.Column(fichas,key='atrilFichas',justification='center',visible=False)],
@@ -150,7 +192,8 @@ layout=[
        [sg.Column(opciones_de_juego,key='opcionesJuego',justification='center',visible=False)],   
        ]
 
-window = sg.Window('SCRABBLE', layout, default_button_element_size=(2,2), auto_size_buttons=False)
+window = sg.Window('SCRABBLE', layout, default_button_element_size=(2,2),finalize=True, resizable=True,  auto_size_buttons=True)
+
 
 a=[]#letras que voy usando
 usados = []#lleva las letras que ya use
@@ -179,124 +222,84 @@ while True:
          if event in  ("Boton_1","Boton_2","Boton_3","Boton_4","Boton_5","Boton_6","Boton_7") and lugar:#si el evento seria una letra y lugar tiene algo es xq marque algo del tabler
                  letra = window[event].GetText()  #asigno la letra del evento
                  if lugar not in no_disponibles: #si el lugar no lo use
-                     #usados.append(letra) #lo agrega a mi lista de usados
                      print(letra)
                      if len(usados)==0: #vemos si es la primera letra, seteamos la orientacion de la palabra
-                        vertical=False
-                        horizontal=False
-                        usados.append(letra) #lo agrega a mi lista de usados
-                        botones_usados.append(event)
-                        window[lugar].update(letra, button_color=('white','green'))#pinto de verde
+                        letra_al_tablero(window,usados,botones_usados,a,no_disponibles)
+                        vertical=horizontal=False
                         box_X_vertical=box_X_horizontal=lugar[1]#estas variables sirven para guardar la cord X horizontal y vertical anterior
                         box_Y_vertical=box_Y_horizontal=lugar[0]#estas variables sirven para guardar la cord Y horizontal y vertical anterior
-                        a.remove(letra) #saco la letra de la bolsa
-                        #print(a) #PARA TESTEO
-                        window[event].update(visible = False) #saco el boton de esa letra
-                        no_disponibles.append(lugar)#cargo el lugar que ya use
                      elif len(usados)==1: #vemos si es la primera letra, seteamos la orientacion de la palabra    
                          if box_X_horizontal+1==lugar[1] and box_Y_horizontal==lugar[0]:
-                            print('es horizontal')  
                             horizontal=True
-                            usados.append(letra) #lo agrega a mi lista de usados
-                            botones_usados.append(event)
-                            window[lugar].update(letra, button_color=('white','green'))#pinto de verde
+                            letra_al_tablero(window,usados,botones_usados,a,no_disponibles)
                             box_X_horizontal=lugar[1]
                             box_Y_horizontal=lugar[0]
-                            a.remove(letra) #saco la letra de la bolsa
-                            #print(a) #PARA TESTEO
-                            window[event].update(visible = False) #saco el boton de esa letra
-                            no_disponibles.append(lugar)  #cargo el lugar que ya use
                          elif box_X_vertical==lugar[1] and box_Y_vertical+1==lugar[0]:
-                            print('es vertical')  
                             vertical=True 
-                            usados.append(letra) #lo agrega a mi lista de usados
-                            botones_usados.append(event)
-                            window[lugar].update(letra, button_color=('white','green'))#pinto de verde
+                            letra_al_tablero(window,usados,botones_usados,a,no_disponibles)
                             box_X_vertical=lugar[1]
                             box_Y_vertical=lugar[0]
-                            a.remove(letra) #saco la letra de la bolsa
-                            #print(a) #PARA TESTEO
-                            window[event].update(visible = False) #saco el boton de esa letra
-                            no_disponibles.append(lugar)  #cargo el lugar que ya use
                      elif len(usados)>1:      
                          if vertical:
-                             #print('es verti')  #PARA TESTEO
-                             #print( box_Y_vertical+1 ,' y ',lugar[0])#PARA TESTEO
-                             if box_X_vertical==lugar[1] and box_Y_vertical+1==lugar[0]:
-                                 #print( box_Y_vertical+1 ,' y ',lugar[0])#PARA TESTEO
-                                 #print(letra)#PARA TESTEO
-                                 usados.append(letra) #lo agrega a mi lista de usados
-                                 botones_usados.append(event)
-                                 window[lugar].update(letra, button_color=('white','green'))#pinto de verde
-                                 box_X_vertical=lugar[1]
-                                 box_Y_vertical=lugar[0]
-                                 a.remove(letra) #saco la letra de la bolsa
-                                 #print(a) #PARA TESTEO
-                                 window[event].update(visible = False) #saco el boton de esa letra
-                                 no_disponibles.append(lugar)  #cargo el lugar que ya use
-                             else:
-                                 sg.Popup('Lugar Invalido')  
-                                  
+                                if box_X_vertical==lugar[1] and box_Y_vertical+1==lugar[0]:
+                                    letra_al_tablero(window,usados,botones_usados,a,no_disponibles)
+                                    box_X_vertical=lugar[1]
+                                    box_Y_vertical=lugar[0]
+                                else:
+                                    sg.Popup('Lugar Invalido') 
                          elif horizontal:
-                            #print('es hori') #PARA TESTEO
-                            #print( 'A',box_X_horizontal+1 ,' y ',lugar[1])#PARA TESTEO
-                            if box_X_horizontal+1==lugar[1] and box_Y_horizontal==lugar[0]:
-                                #print( 'B',box_X_horizontal+1 ,' y ',lugar[1])#PARA TESTEO
-                                #print(letra)#PARA TESTEO
-                                usados.append(letra) #lo agrega a mi lista de usados
-                                botones_usados.append(event)
-                                window[lugar].update(letra, button_color=('white','green'))#pinto de verde
-                                box_X_horizontal=lugar[1]
-                                box_Y_horizontal=lugar[0]
-                                a.remove(letra)#saco la letra de la bolsa
-                                #print(a)#PARA TESTEO
-                                window[event].update(visible = False)#saco el boton de esa letra
-                                no_disponibles.append(lugar) #cargo el lugar que ya use
-                            else:
-                                sg.Popup('Lugar Invalido')      
+                                if box_X_horizontal+1==lugar[1] and box_Y_horizontal==lugar[0]:
+                                    letra_al_tablero(window,usados,botones_usados,a,no_disponibles)
+                                    box_X_horizontal=lugar[1]
+                                    box_Y_horizontal=lugar[0]     
+                                else:
+                                    sg.Popup('Lugar Invalido')  
                      print('Letras de atril despues de cargar:',a)
                      print(len(usados),' ',len(no_disponibles))              
-        
-         #print(event)
-         if event == "Pedir Fichas":
-             a = letrasRandom()
-             #ZIP lo que hace es crear una lista de tuplas con las listas que le pasas
-             mezcla = zip(usados,a)
-             print(mezcla)
-             for elem in mezcla:
-                 #busco la letra que use,en en ese lugar hago visible el boton y actualizo su texto
-                 window[elem[0]].update(elem[1],visible=True)
-        
-         elif event == "Repartir De Nuevo":
+
+         if event == "Pedir Fichas": #NO FUNCIONA , pide fichas hasta llegar a 7 en la mano
+            pedir_fichas(window,usados)
+            
+         elif event == "Repartir De Nuevo": #pide 7 fichas nuevas en la mano
             cantidad_de_veces_Repartidas=repartir_fichas_de_nuevo(window,cantidad_de_veces_Repartidas,a) 
             
          elif event == "Borrar" : #quita elementos del tablero, desde el ultimo al primero  
-             quitar_fichas(window,usados,botones_usados,no_disponibles)
+            quitar_fichas(window,usados,botones_usados,no_disponibles)
        
-         elif event == "Comenzar":
-             asignarValores(window)
-             window['opcionesJuego'].update(visible=True)
-             window['Comenzar'].update(visible=False)
-             window['Cargar Partida'].update(visible=False)
-             window['Guardar Partida'].update(visible=True)
-             window['Salir'].update(visible=True)
-             window['puntaje'].update(visible=True)
-             window['atrilFichasRival'].update(visible=True)
-             window['atrilFichas'].update(visible=True)
-             for i in range(7):#carga de las 7 fichas al inicio
-                nro_de_boton='Boton_'+str(i+1)
-                obtener_fichas(window,nro_de_boton,a)
-             timer_running = not timer_running
+         elif event == "Comenzar": # para inicializar el juego
+             timer_running=cargar_juego(window,timer_running)
 
+         elif event == "TOP":
+            tab1_layout = [[sg.T('This is inside tab 1')]]    
+            tab2_layout = [[sg.T('This is inside tab 2')],[sg.In(key='in')]]    
+            tab3_layout = [[sg.T('This is inside tab 3')]]    
+            tab4_layout = [[sg.T('This is inside tab 4')]]   
+            layout2 = [
+                        [sg.Text('RANKING'), sg.Text('', key='_OUTPUT_')],
+                        [sg.TabGroup([
+                                        [sg.Tab('Ranking General', tab1_layout, tooltip='tip'), 
+                                         sg.Tab('Ranking Facil', tab2_layout, tooltip='tip2'),   
+                                         sg.Tab('Ranking Medio', tab3_layout, tooltip='tip3'),  
+                                         sg.Tab('Ranking Medio', tab4_layout,tooltip='tip4')]
+                                    ])],
+                        [sg.Button('Exit')]
+                      ]
+            window2 = sg.Window('TOP TEN').Layout(layout2)
+            while True:
+                event2, values2 = window2.Read()
+                if event2 == 'Exit':
+                    break
 
-         elif timer_running:
-           #  window['-OUTPUT-'].update('{:02d}:{:02d}'.format((counter // 100) // 60, (counter // 100) % 60, counter % 100))
+            window2.Close()  
+
+         elif timer_running: #esto es para que corra el tiempo
+             #  window['-OUTPUT-'].update('{:02d}:{:02d}'.format((counter // 100) // 60, (counter // 100) % 60, counter % 100))
              window['-OUTPUT-'].update('{:02d}:{:02d}'.format((counter // 100) // 60, (counter // 100) % 60))
              counter += 1
-             if counter==60000:#600 equivale a 1 minuto, 60000 a 10 minutos
+             print(counter)
+             if counter==6000:#6000 equivale a 1 minuto, 60000 a 10 minutos
                 timer_running = not timer_running
-                sg.Popup('termino el tiempo')       
-
+                sg.Popup('termino el tiempo')        
 
          elif event == "Evaluar": #aca va evaluar,evalua la palabra y resetea las orientaciones
              vertical = False
@@ -330,8 +333,9 @@ while True:
                      #busco la letra que use,en en ese lugar hago visible el boton y actualizo su texto
                      window[elem[0]].update(elem[1],visible=True)
 
+
+
     window.Refresh()         
-    window.Refresh()
     #window.Size=window.Size
 #update(atrilNuevo[i])
 window.close()
