1566cb727eb4568c49693b04739d52fcb63a2eb8
Lucas Di Lorenzo <lucasdilorenzo@Lucass-MacBook-Pro.local>
2020-06-29 12:11:32 -0300

    comentarios agregados, campo de config tiempo agregado , y nuevo dibujo de tablero en el config

--- ScrabbleAR.py
+++ ScrabbleAR.py
@@ -11,34 +11,46 @@ def configuracion_de_juego():
         configuracion del juego inicial,cargamos el config.json, para poder cambiar
         la cantidades de letras iniciales en la bolsa de fichas y ademas podemos 
         editar el valor de cada ficha
-    """    
+    """ 
     with open('config.json','r') as cf:
         c = json.load(cf)
-
+        
     diccionario_cantidad_de_letras=c['cantidad_de_letras']
     diccionario_cantidad_de_puntos=c['valor_por_letra']
   
-
     layout3=[
-                [sg.Text("Configuracion", size=(8, 1), justification='left', font=("Chalkboard", 25), relief=sg.RELIEF_RIDGE)],
-                [sg.Text(k,size=(5,1),justification='center')for k,v in diccionario_cantidad_de_letras.items()],
+                [sg.Text("Configuracion", size=(10, 1), justification='left', font=("Chalkboard", 25), relief=sg.RELIEF_RIDGE)],
+                [sg.Text(k,size=(3,1),justification='center')for k,v in diccionario_cantidad_de_letras.items()],
                 [sg.Text('PARA MODIFICAR LA CANTIDAD DE LETRAS POR LETRA CAMBIE EL VALOR A LA LETRA CORRESPONDIENTE :')],
-                [sg.Input(v,size=(5,1),key=(k+'_cant'))for k,v in diccionario_cantidad_de_letras.items()],
+                [sg.Input(v,size=(3,1),key=(k+'_cant'))for k,v in diccionario_cantidad_de_letras.items()],
                 [sg.Text('PARA MODIFICAR EL VALOR DE LAS LETRAS POR LETRA CAMBIE EL VALOR A LA LETRA CORRESPONDIENTE:')],
-                [sg.Input(v,size=(5,1),key=(k+'_valor'))for k,v in diccionario_cantidad_de_puntos.items()],
+                [sg.Input(v,size=(3,1),key=(k+'_valor'))for k,v in diccionario_cantidad_de_puntos.items()],
+                [sg.Text('')],
+                [sg.Text('PARA MODIFICAR EL TIEMPO INGRESE EL TIEMPO EN MINUTOS (de 5 a 30) QUE QUIERES QUE DURE LA PARTIDA')],
+                [sg.Input(size=(9,1),key=('time'))],
                 [sg.Button('GUARDAR CONFIGURACION'),sg.Button('CANCELAR')]
             ]
 
     window3 = sg.Window('Configuracion').Layout(layout3)
     while True:
         event3, values3 = window3.Read()
-        
+    
         if event3 == 'CANCELAR':
             break
 
         if event3 == 'GUARDAR CONFIGURACION':
             #with open('config.json','w') as cf:
-
+            
+            try:
+                num_tiempo=int(values3['time'])
+                if ( num_tiempo > 30 ):
+                    num_tiempo=30
+                if ( num_tiempo < 5 ):
+                    num_tiempo=5    
+            except (ValueError):
+                    num_tiempo= 15 # seteamos 15 minutos en caso de cargar erronea, pero informamos que se cargo mal el tiempo
+                    sg.Popup('El tiempo se cargo de maneara erronea, pero cargamos la partida de 15 minutos')
+                   
             for k,v in diccionario_cantidad_de_letras.items():
                 #print('valores: ',values3[k+'_cant'])
                 #print (type(values3[k+'_cant']))  
@@ -46,18 +58,19 @@ def configuracion_de_juego():
                     numero_nuevo=int(values3[k+'_cant'])
                 except (ValueError):
                     numero_nuevo=1   
-                #print(type(numero_nuevo))
+                print(numero_nuevo)
+            sg.Popup('La duracion de la partida sera de: ',num_tiempo,' minutos')
+            
                 
 
     window3.close()    
 
       
-
-
-
-
-#crear bolsas para maquina y jugador
 def crear_bolsita_total():
+    """
+        Generamos la bolsas con las fichas totales para usar en el juego, 
+        las cuales tenemos almacenadas en el config.json
+    """
     with open('config.json','r') as cf:
         c = json.load(cf)
     bolsa = c['cantidad_de_letras']
@@ -65,24 +78,25 @@ def crear_bolsita_total():
 
 
 def crear_atril(bolsa_total):
-#    print(bolsa_total,'antes')
+    """
+
+    """
+    #print(bolsa_total,'antes')
     letra = random.choice(list(bolsa_total.keys()))
     while bolsa_total[letra] == 0:
             letra= random.choice(list(bolsa_total.keys()))
     bolsa_total[letra] -= 1
     #print(letra,'letra retirada')
-#    print(bolsa_total,'despues')
+    #print(bolsa_total,'despues')
     return letra
 
 
-
-#carga de nickname previa a jugar, solo para guardar el dato en el ranking y que muestre su puntaje
 def carga_nombre():
-    ''' 
+    """ 
         Cargamos el nombre al ingresar al juego para usarlo luego al cargar los
         datos de la partida, tambien lo utilizamos para que indique el puntaje 
         del jugardor activo en el momento
-    '''    
+    """   
     layout3=[[sg.Text('Ingresa tu nombre:')],
             [sg.Input('',key='name')],
             [sg.Button('Listo')]]
@@ -95,13 +109,13 @@ def carga_nombre():
     window3.close()
     return nombre
 
-#pinta el tablero segun la dificultad
-def asignarValores2(window,dificultad):
-    '''
+
+def asignar_colores_al_tablero(window,dificultad):
+    """
        Se van a pintar los botones de la matriz que conforman el tablero
        segun las coordenas que vengan en el archivo json, dependiendo 
        el nivel que hayamos elegido
-    ''' 
+    """ 
     with open('config.json','r') as t:
         dic = json.load(t)
 
@@ -114,17 +128,18 @@ def asignarValores2(window,dificultad):
             x,y = par_de_cord
             window[x,y].update(button_color=(colores,colores))
 
+
 def cargar_juego(window,timer_running,nombre,bolsa_total):
-    '''
+    """
         iniciamos todo el seteo inicial del juego: 
             -la dificultad segun la seleccionada en el menu
             -Pintamos el tablero segun la dificultad
             -Ocultamos los botones de Comenzar, Cargar Partida
             -Volvemos visibles los botones de Guardar, Salir
             -Volvemos visibles las opciones de juego para poder jugar
-    ''' 
+    """
     very_dificult=values['dificultad']
-    asignarValores2(window,very_dificult)
+    asignar_colores_al_tablero(window,very_dificult)
     window['opcionesJuego'].update(visible=True)
     window['Comenzar'].update(visible=False)
     window['Configuracion'].update(visible=False)
@@ -146,12 +161,11 @@ def cargar_juego(window,timer_running,nombre,bolsa_total):
     return timer_running, very_dificult
 
 
-#para volver a colorear una casilla ya usada
-def volverAPintar(cord,window):
-    '''
+def volver_a_pintar_la_casilla(cord,window):
+    """
         este modulo nos sirve para volver a pintar un boton de su color inicial 
         en caso de que tengamos que sacar una ficha que pusimos
-    '''
+    """
     with open ('config.json','r') as p:
         dicc = json.load(p)
     #la dific tiene que vernir como parametro para saber que tablero abrir
@@ -163,11 +177,11 @@ def volverAPintar(cord,window):
     #elif list(cord) in tablero_actual["grey"]: window[cord].update(button_color=('grey','grey'))
     else: window[cord].update(button_color=('grey','white'))
 
-#verifica si es o no una palabra
+
 def verificar_palabra(palabra):
-    '''
+    """
         verificamos si la palabra es valida
-    '''
+    """
     if palabra in lexicon and spelling or palabra in verbs:
         return(True)
     else:
@@ -175,20 +189,24 @@ def verificar_palabra(palabra):
 
 
 def obtener_fichas(window,nro_de_boton,a,bolsa_total):
-    '''
+    """
         Este metodo nos da una ficha "nueva".
         Generamos una letra random la agregamos a la lista de letras de nuestro atril,
         luego con la key del boton recibida como parametro actualizamos el valor del 
         boton con la nueva letra
-    '''
+    """
     letra=crear_atril(bolsa_total)
     a.append(letra)
     window[nro_de_boton].update(letra)
     window[nro_de_boton].update(button_color=('black','oldlace'))
     window.Refresh()
 
-#reparte una mano
+
 def repartir_fichas_de_nuevo(window,cantidad_de_veces_Repartidas,a):
+    """
+        Utilizamos este metodo para poder cambiar la mano de fichas que tenemos,
+        y tenemos permitido hacerlo hasta 3 veces
+    """
     a=[]
     if (cantidad_de_veces_Repartidas < 3):
         cantidad_de_veces_Repartidas=cantidad_de_veces_Repartidas+1
@@ -201,18 +219,18 @@ def repartir_fichas_de_nuevo(window,cantidad_de_veces_Repartidas,a):
 
 
 def quitar_fichas(window,usados:list,botones_usados:list,no_disponibles:list):
-    '''
+    """
         quitar fichas nos permite scara las fichas ingresadas al tablero en el turno correspondiente.
         Mientras que la longitud lista de usados del turno sea mayor a 0 vamos a poder retirar fichas
         que ingresamos, en caso de que no se pueda no podremos quitar mas
-    '''
+    """
     #print(len(usados),' ',len(no_disponibles),' ',len(botones_usados))
     if len(usados)>0:# Aca antes de borrar una letra vamos a preguntar si hay letras para borrar, en caso contrario no podras borrar mas letras
         letra_a_borrar=usados.pop(len(usados) - 1)# Saca la ultima letra de la palabra cargada - el pop saca de usados  el elemento de la ultima posicion de la lista de usados
         boton_a_recuperar= botones_usados.pop(len(botones_usados)-1)
         a.append(letra_a_borrar)# vuelve a cargar la letra que sacamos del tablero en el atril de nuestras fichas
         coord_a_liberar=no_disponibles.pop(len(no_disponibles) - 1)# Saca la ultima coordenada de la palabra cargada - el pop saca de no_disponibles el elemento de la ultima posicion de la lista de no_disponibles
-        volverAPintar(coord_a_liberar,window) #vuelve a pintar la casilla de su color en estado inicial
+        volver_a_pintar_la_casilla(coord_a_liberar,window) #vuelve a pintar la casilla de su color en estado inicial
         window[coord_a_liberar].update("")
         window[boton_a_recuperar].update(button_color=('black','oldlace'),disabled=False)
     else:
@@ -220,6 +238,10 @@ def quitar_fichas(window,usados:list,botones_usados:list,no_disponibles:list):
 
 
 def pedir_fichas(window,botones_usados,a,bolsa_total):
+    """
+       El pedir fichas nos permite pedir la cantidad de fichas usadas en el ultimo turno 
+       hasta llegar a tener 7 nuevamente. 
+    """
     for i in botones_usados:
         letra=a[len(a)-1]
         window[i].update(letra,disabled = False)
@@ -229,16 +251,26 @@ def pedir_fichas(window,botones_usados,a,bolsa_total):
 
 
 def letra_al_tablero(window,usados,botones_usados,a,no_disponibles):
-    usados.append(letra) #lo agrega a mi lista de usados
-    botones_usados.append(event)#agrego el nombre del boton para luego recuperarlo
-    window[lugar].update(letra, button_color=('black','oldlace'))#pinto de verde
-    a.remove(letra) #saco la letra de la bolsa
-    window[event].update(button_color=('darkgrey','darkgrey'),disabled = True) #saco el boton de esa letra
-    no_disponibles.append(lugar)#cargo el lugar que ya use
+    """
+       Utilizamos este metodo para graficar el colocar la ficha en el tablero.
+       -Guardamos la ficha en una lista de "usados"
+       -Guardamos el nombre del boton para luego recuperarlo 
+       -En el tablero ponemos el valor de la ficha seleccionada
+       -Sacamos la letra de nuestro atril, y la deshabilitamos del atril
+       -Para finalizar guardamos las coordenadas del lugar en una lista de lugares no disponibles
+    """
+    usados.append(letra) 
+    botones_usados.append(event)
+    window[lugar].update(letra, button_color=('black','oldlace'))
+    a.remove(letra)
+    window[event].update(button_color=('darkgrey','darkgrey'),disabled = True) 
+    no_disponibles.append(lugar)
 
-#calcula el valor de 1 letra
+
 def puntos_de_letra(letra,dificultad,coord):
-
+    """
+       calculamos el valor de la letra ingresada en el tablero. accedemos al json para ver su puntaje asociado
+    """
     with open ('config.json','r') as p:
         dicc = json.load(p)
     valor_de_letra = dicc["valor_por_letra"]
@@ -256,7 +288,13 @@ def puntos_de_letra(letra,dificultad,coord):
     else:
         return valor_de_letra[letra]
 
+
 def puntos_de_palabra(dificultad,no_disponibles,puntos):
+    """
+       En esta parte vamos a calcular el total de los puntos de la palabra cargada.
+       Segun la dificultad vamos a tener distintos modificadores en cada color
+       Si nuestra palabra pasa por casillas con colores el puntaje final se vera afectado
+    """
     with open ('config.json','r') as p:
         dicc = json.load(p)
     tablero_actual = dicc[dificultad]
@@ -278,9 +316,7 @@ def puntos_de_palabra(dificultad,no_disponibles,puntos):
 
 bolsa_total = crear_bolsita_total()
 
-atril_maquina = crear_atril(bolsa_total)  #el de la maquina
-
-
+atril_maquina = crear_atril(bolsa_total) 
 
 color_De_Boton=('Black','seagreen')
 tamanio_Boton_De_Fichas = 2,2 #tamanio de botones que seran las fichas
@@ -339,16 +375,16 @@ layout=[
 
 window = sg.Window('SCRABBLE', layout, default_button_element_size=(2,2),finalize=True, resizable=True,  auto_size_buttons=True)
 
-puntos = 0; #puntos del jugdador
-a=[]#letras que voy usando
-usados = []#lleva las letras que ya use
-botones_usados=[]#nombre de los botones que voy usando
-no_disponibles = []#lleva la cuenta de los lugares que ya escribi
-ant = ()#para despintar la casilla anterior cuando toco una nueva
+puntos = 0 # puntos del jugdador
+a=[] # letras que voy usando
+usados = [] # lleva las letras que ya use
+botones_usados=[] # nombre de los botones que voy usando
+no_disponibles = [] # lleva la cuenta de los lugares que ya escribi
+ant = () # para despintar la casilla anterior cuando toco una nueva
 lugar = ()
 layout2 = layout
-cantidad_de_veces_Repartidas=0#cantidad de veces de pedidos para hacer el cambio de fichas totales
-timer_running, counter = False, 0 #seteos para el timer,
+cantidad_de_veces_Repartidas=0 # cantidad de veces de pedidos para hacer el cambio de fichas totales
+timer_running, counter = False, 0 # seteos para el timer,
 dificult=''
 
 while True:
@@ -361,7 +397,7 @@ while True:
              if lugar not in no_disponibles: #pinto el lugar que estoy seleccionando,hago esa pregunta para que no trate de marcar un casillero que ya tiene una letra
                  window[lugar].update(button_color=('white','darkgrey'))
              if (ant) and (ant not in no_disponibles): #digo que si anterior tiene algo que despinte lo anterior
-                 volverAPintar(lugar,window)
+                 volver_a_pintar_la_casilla(lugar,window)
              ant = lugar,
 
          if event in  ("Boton_1","Boton_2","Boton_3","Boton_4","Boton_5","Boton_6","Boton_7") and lugar:#si el evento seria una letra y lugar tiene algo es xq marque algo del tabler
