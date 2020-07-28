6a87f376da7376bc003641a6e40a515ec05f4a7b
Tindilp <62031847+Tindilp@users.noreply.github.com>
2020-07-28 01:11:27 -0300

    confittes

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -11,37 +11,41 @@ def configuracion_de_juego():
         cambiar la cantidades de letras iniciales en la bolsa de fichas y
         ademas podemos editar el valor de cada ficha
     """
-    with open('config.json', 'r') as cf:
+    with open('./texto/config.json', 'r') as cf:
         c = json.load(cf)
 
     diccionario_cantidad_de_letras = c['cantidad_de_letras']
     diccionario_cantidad_de_puntos = c['valor_por_letra']
 
     layout3 = [
-        [sg.Text("Configuracion", size=(10, 1), justification='left',
-                 font=("Chalkboard", 25), relief=sg.RELIEF_RIDGE)],
-        [sg.Text(k, size=(3, 1), justification='center')
-         for k, v in diccionario_cantidad_de_letras.items()],
-        [sg.Text('PARA MODIFICAR LA CANTIDAD DE LETRAS POR LETRA CAMBIE EL ',
-                 'VALOR A LA LETRA CORRESPONDIENTE :')],
-        [sg.Input(v, size=(3, 1), key=(k+'_cant'))
-         for k, v in diccionario_cantidad_de_letras.items()],
-        [sg.Text('PARA MODIFICAR EL VALOR DE LAS LETRAS POR LETRA CAMBIE EL ',
-                 'VALOR A LA LETRA CORRESPONDIENTE:')],
-        [sg.Input(v, size=(3, 1), key=(k+'_valor'))
-         for k, v in diccionario_cantidad_de_puntos.items()],
-        [sg.Text('')],
-        [sg.Text('PARA MODIFICAR EL TIEMPO INGRESE EL TIEMPO EN MINUTOS ',
-                 '(de 5 a 30) QUE QUIERES QUE DURE LA PARTIDA')],
+        [sg.Text("Configuracion", size=(10, 1), justification='left',font=("Chalkboard", 25), relief=sg.RELIEF_RIDGE)],
+        [sg.Text(k, size=(3, 1), justification='center') for k, v in diccionario_cantidad_de_letras.items()],
+        [sg.Text('PARA MODIFICAR LA CANTIDAD DE LETRAS POR LETRA CAMBIE EL VALOR A LA LETRA CORRESPONDIENTE :')],
+        [sg.Input(v, size=(3, 1), key=(k+'_cant')) for k, v in diccionario_cantidad_de_letras.items()],
+        [sg.Text('PARA MODIFICAR EL VALOR DE LAS LETRAS POR LETRA CAMBIE EL VALOR A LA LETRA CORRESPONDIENTE:')],
+        [sg.Input(v, size=(3, 1), key=(k+'_valor')) for k, v in diccionario_cantidad_de_puntos.items()],
+        [sg.Text('')],[sg.Text('PARA MODIFICAR EL TIEMPO INGRESE EL TIEMPO EN MINUTOS (de 5 a 30) QUE QUIERES QUE DURE LA PARTIDA')],
         [sg.Input(size=(9, 1), key=('time'))],
-        [sg.Button('GUARDAR CONFIGURACION'), sg.Button('CANCELAR')]
+        [sg.Button('CONFIGURACION POR DEFECTO'), sg.Button('GUARDAR CONFIGURACION'), sg.Button('CANCELAR')]
     ]
 
     window3 = sg.Window('Configuracion').Layout(layout3)
+    print('tests')
     while True:
+
         event3, values3 = window3.Read()
-        if event3 == 'CANCELAR':
+
+        if event3 == 'CANCELAR' or event3 == None:
             break
+
+        if event3 == 'CONFIGURACION POR DEFECTO':
+            with open('./texto/config_default.json', 'r') as cf:
+                c = json.load(cf)
+            with open('./texto/config.json', 'w') as cf:
+                json.dump(c,cf, indent=4)    
+            sg.popup('Se restauro la configuracion inicial :D')    
+            break
+
 
         if event3 == 'GUARDAR CONFIGURACION':
             # with open('config.json','w') as cf:
@@ -52,17 +56,32 @@ def configuracion_de_juego():
                 if (num_tiempo < 5):
                     num_tiempo = 5
             except (ValueError):
-                # seteamos 15 minutos en caso de cargar erronea de tiempo
-                num_tiempo = 15
+                # seteamos 10 minutos en caso de cargar erronea de tiempo
+                num_tiempo = 10
                 sg.Popup('El tiempo se cargo de maneara erronea, pero ',
-                         'cargamos la partida de 15 minutos')
+                         'cargamos la partida de 10 minutos')
+            
             for k, v in diccionario_cantidad_de_letras.items():
+
                 try:
-                    numero_nuevo = int(values3[k+'_cant'])
+                    diccionario_cantidad_de_letras[k]= int(values3[k+'_cant'])
                 except (ValueError):
-                    numero_nuevo = 1
-            sg.Popup('La duracion de la partida sera de: ',
-                     num_tiempo, ' minutos')
+                    diccionario_cantidad_de_letras[k] = 1
+                try:
+                    diccionario_cantidad_de_puntos[k]= int(values3[k+'_valor'])
+                except (ValueError):
+                    diccionario_cantidad_de_puntos[k] = 1    
+
+            with open('./texto/config.json', 'w') as cf:
+                c['cantidad_de_letras']=diccionario_cantidad_de_letras 
+                c['valor_por_letra']=diccionario_cantidad_de_puntos 
+                c['tiempo']= num_tiempo   
+                json.dump(c,cf, indent=4)   
+                
+            sg.Popup('La duracion de la partida sera de: ',num_tiempo, ' minutos')
+            sg.popup('Se configuro la partida')
+            break              
+
     window3.close()
 
 
